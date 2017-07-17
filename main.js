537f9eab5987fcb10c005abfa7494938cc937097
Leonard <leonard.horne@gmail.com>
2017-07-17 14:56:30 -0400

    Delineate todos.

---
+++ main.js
@@ -0,0 +1,6 @@
+// TODO: use template literal to make main (with class of container); center h1; two h2 (class of header), two p tags (width: 35% + 65% respectively to start). p tags are flex items (display:flex on container); the story p tag has lightgrey border-left  
+// TODO: examine github api data
+// TODO: create AJAX request to pull info from personal profile and add to p tag
+
+
+let vCard = document.querySelector('body');
