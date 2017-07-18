00003e092806fd8e4d4ee38a81b6ad35f7e00bd1
Leonard <leonard.horne@gmail.com>
2017-07-18 10:13:21 -0400

    Change corporate ipsum to hipster ipsum.

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function vCard() {
         </section>
         <section class="story">
           <h2 class="subheader">The Story</h2>
-          <p id ="bio">Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment. Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p >
+          <p id ="bio">Wolf waistcoat franzen four dollar toast. Listicle plaid try-hard normcore mustache. Beard lo-fi waistcoat, wolf heirloom lomo raw denim cray four dollar toast hoodie organic thundercats. Chicharrones knausgaard migas umami iPhone cliche whatever single-origin coffee drinking vinegar readymade shoreditch mustache pop-up blog subway tile. Retro lomo vinyl readymade truffaut normcore offal snackwave quinoa pop-up semiotics fashion axe selfies. Street art truffaut vice art party.</p >
         </section>
         <section>
           <img src="" alt="">
@@ -27,6 +27,8 @@ function vCard() {
 
   container.innerHTML = mainContent;
 };
+
+
 
 let githubDataReq = new XMLHttpRequest();
 githubDataReq.open("GET", "https://api.github.com/users/llhdc");
