b74fa5e7f9760810e436cc184eaeb6390a9070de
Leonard <leonard.horne@gmail.com>
2017-07-17 20:06:18 -0400

    Activate flex-box. Style content divs.

--- main.js
+++ main.js
@@ -6,34 +6,25 @@ let container = document.querySelector('body');
 function vCard() {
   let data = JSON.parse(this.responseText);
   let mainContent = `
+        <main>
         <h1></h1>
-        <div>
+        <div class="bio">
         <h2 class="subheader"></h2>
         <ul>
           <li><span id="name">Name: ${data.name}</span></li>
           <li><span id="url">${data.url}</span></li>
-          <li><span id="company"></span></li>
-          <li><span id=""></span></li>
+          <li><span id="company">${data.company}</span></li>
+          <li><span id="website">${data.blog}</span></li>
         </ul>
         </div>
         <div>
           <h2 class="subheader"></h2>
           <p>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment. Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>
         </div>
+        </main>
       `;
 
   container.innerHTML = mainContent;
-
-
-  // let url = document.createTextNode(data.url)
-  //
-  // let urlSpan = document.getElementById('url')
-  // let urlSpanContent = document.createTextNode(data.url)
-  // urlSpan.appendChild(urlSpanContent);
-
-  let companySpan = document.getElementById('company')
-  let companySpanContent = document.createTextNode(data.company)
-  companySpan.appendChild(companySpanContent);
 };
 
 let githubDataReq = new XMLHttpRequest();
